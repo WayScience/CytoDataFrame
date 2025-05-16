5b22ff9a1c083cedd29a4ac913aca8efe241a15b
d33bs <ekgto445@gmail.com>
2025-05-16 13:41:59 -0600

    linting

--- docs/src/examples/cytodataframe_at_a_glance.py
+++ docs/src/examples/cytodataframe_at_a_glance.py
@@ -49,9 +49,7 @@ CytoDataFrame(
         "Image_FileName_OrigDNA",
         "Image_FileName_OrigRNA",
     ]
-][
-    :3
-]
+][:3]
 
 # %%time
 # view JUMP plate BR00117006 with images and overlaid outlines for segmentation
@@ -67,9 +65,7 @@ CytoDataFrame(
         "Image_FileName_OrigDNA",
         "Image_FileName_OrigRNA",
     ]
-][
-    :3
-]
+][:3]
 
 
 # %%time
@@ -87,9 +83,7 @@ CytoDataFrame(
         "Image_FileName_OrigDNA",
         "Image_FileName_OrigRNA",
     ]
-][
-    :3
-]
+][:3]
 
 # %%time
 # view JUMP plate BR00117006 with images, change the display height and width
@@ -107,9 +101,7 @@ CytoDataFrame(
         "Image_FileName_OrigDNA",
         "Image_FileName_OrigRNA",
     ]
-][
-    :5
-].T
+][:5].T
 
 # %%time
 # view NF1 Cell Painting data with images
@@ -124,9 +116,7 @@ CytoDataFrame(
         "Image_FileName_RFP",
         "Image_FileName_DAPI",
     ]
-][
-    :3
-]
+][:3]
 
 # %%time
 # view NF1 Cell Painting data with images and overlaid outlines from masks
@@ -142,9 +132,7 @@ CytoDataFrame(
         "Image_FileName_RFP",
         "Image_FileName_DAPI",
     ]
-][
-    :3
-]
+][:3]
 
 # +
 # %%time
@@ -172,9 +160,7 @@ CytoDataFrame(
         "Image_FileName_RFP",
         "Image_FileName_DAPI",
     ]
-][
-    :3
-]
+][:3]
 # -
 
 # %%time
@@ -191,9 +177,7 @@ CytoDataFrame(
         "Image_FileName_DAPI",
         "Image_FileName_GOLD",
     ]
-][
-    :3
-]
+][:3]
 
 # %%time
 # view ALSF pediatric cancer atlas plate BR00143976 with images
